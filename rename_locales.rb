19ef0369756dae7e3a8197d77dab1c6f6fc20178
Jonne Haß <me@mrzyx.de>
2012-02-16 19:03:22 +0100

    added locale mapping script for wti

---
+++ rename_locales.rb
@@ -0,0 +1,39 @@
+#!/usr/bin/env ruby
+
+require 'yaml'
+require 'fileutils'
+
+locale_code = /[\w\d_-]+/
+directories = {
+  "config/locales/devise/devise.*.yml" => /devise\.(#{locale_code})\.yml/,
+  "config/locales/diaspora/*.yml" => /(#{locale_code})\.yml/,
+  "config/locales/javascript/javascript.*.yml" => /javascript\.(#{locale_code})\.yml/,
+}
+
+mappings = YAML.load_file('.wti')['mappings']
+
+def flip_direction?
+  ARGV[0] && ARGV[0] == "revert"
+end
+
+mappings.each do |from, to|
+  if flip_direction?
+    tmp = to
+    to = from
+    from = tmp
+  end
+  
+  directories.each do |filepattern, regex|
+    Dir[filepattern].each do |file|
+      filename = File.basename file
+      
+      if filename =~ regex
+        if $1 == from
+          dst = file.gsub(from, to)
+          puts "Rename #{file} to #{dst}"
+          FileUtils.mv file, dst
+        end
+      end
+    end
+  end
+end
--- update-translations.sh
+++ update-translations.sh
@@ -3,18 +3,18 @@ location=$(dirname $(readlink -e $0))
 
 if [ ! -d "./config/locales" ]; then
     echo "./config/locales not found"
-    return 1
+    exit 1
 fi
 
 if [ ! -d ".git" ]; then
     echo "Not a git repository"
-    return 2
+    exit 2
 fi
 
 if [ "$1" = "abort" ]; then
     git checkout master
     git reset origin/master --hard
-    return 0
+    exit 0
 fi
 
 # Load right ruby env if needed
@@ -26,8 +26,8 @@ local_changes=$(git stash)
 git pull origin master
 
 # Pull from from 99translations and clean locale files
-$location/yml-helper/update-from-99translations.py
-$location/yml-helper/clean_untranslated_keys.rb
+$location/update-from-99translations.py
+$location/clean_untranslated_keys.rb
 
 # Update rails translations
 bundle update rails-i18n
@@ -35,13 +35,13 @@ bundle update rails-i18n
 # Commit changes
 git add Gemfile.lock
 git add config/locales
-git commit -am "updated $(git status -s config/locales | wc -l) locale files [ci skip]"
+git commit -m "updated $(git status -s config/locales | wc -l) locale files [ci skip]"
 
 # Pull locales not handled through 99translations
 git pull catalan master
 
 # Check for syntax errors
-$location/yh/yml_check.rb
+$location/yml_check.rb
 
 # Restore local changes if needed
 if [ "$local_changes" != "No local changes to save" ]; then
